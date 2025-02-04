24622a0aeb0d69a9469326faaecc37b891fce5d0
Daniel Mikhail <danielmikhail2810@gmail.com>
2025-02-04 13:27:48 -0500

    New Project setup

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mycompany.covalenceapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login_test/login_test_model.dart
@@ -0,0 +1,16 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import 'login_test_widget.dart' show LoginTestWidget;
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class LoginTestModel extends FlutterFlowModel<LoginTestWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
